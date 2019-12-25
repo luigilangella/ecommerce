9923335aa76aba3fb101251aabe4ad32ac45ab5d
Luigi Langella <luigi76langella@gmail.com>
2019-12-25 22:42:04 +0000

    Update requirements.txt file

--- checkout/models.py
+++ checkout/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from products.models import Product
-# Create your models here.
 
+# Create your models here.
 class Order(models.Model):
     full_name = models.CharField(max_length=50, blank=False)
     phone_number = models.CharField(max_length=20, blank=False)
@@ -14,7 +14,8 @@ class Order(models.Model):
     date = models.DateField()
 
     def __str__(self):
-        return '{0}-{1}-{2}'.format(self.id, self.date, self.full_name)
+        return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
+
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False)
@@ -22,6 +23,8 @@ class OrderLineItem(models.Model):
     quantity = models.IntegerField(blank=False)
 
     def __str__(self):
-        return '{0} {1} @ {2}'.format(self.quantity, self.product.name, self.product.price)
+        return "{0} {1} @ {2}".format(
+            self.quantity, self.product.name, self.product.price)
+
     
     
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'accounts',
     'products',
     'cart',
-    'checkout'
+    'checkout',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -130,6 +131,19 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+AWS_S3_OBJECT_PARAMETERS ={
+    'Expires':'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl':'max-age=94608000',
+}
+AWS_STORAGE_BUCKET_NAME = 'luigi.code.institute'
+AWS_S3_REGION_NAME = 'EU'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
