9f58d9d8e2a5cc3baafcfab734f1aec880e8be4e
Luigi Langella <luigi76langella@gmail.com>
2019-12-25 10:22:22 +0000

    Set up stripe to accept online cards transactions

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from accounts import urls as urls_accounts
 from products import urls as urls_products
+from checkout import urls as urls_checkout
 from cart import urls as urls_cart
 from search import urls as urls_search
 from products.views import all_products
@@ -30,6 +31,7 @@ urlpatterns = [
     url(r'^products/', include(urls_products)),
     url(r'^cart/', include(urls_cart)),
     url(r'^search/', include(urls_search)),
+    url(r'^checkout/', include(urls_checkout)),
     url(r'^media(?P<path>.*)$', static.serve, {'document_root':MEDIA_ROOT}),
 
 ]
